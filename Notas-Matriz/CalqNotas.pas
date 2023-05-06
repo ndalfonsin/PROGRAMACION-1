2bf68790c766e2d676f858c040ce18165391342b
ndalfonsin <nicolasdalfonsin@gmail.com>
2023-05-06 20:19:53 -0300

    upload

--- Notas-Matriz/CalqNotas.pas
+++ Notas-Matriz/CalqNotas.pas
@@ -7,7 +7,7 @@ type
     TV = Array[1..15] of String;
 
 var 
-    VAlumno : TV;
+    VAlumno, Promocionados : TV;
     MNotas : TM;
     i, N, M: byte;
 
@@ -78,12 +78,60 @@ procedure Aprobados(VAlumno : TV; MNotas: TM; M, N : byte);
             end;
     end;
 
-// PROMEDIO
+// PROMEDIO, se calcularan por fila I ingresa en el bucle en el que se llama la funcion
 
+function Promedio(MNotas: TM; i, M:byte):real;
+    var
+        j, acum:byte;
+        
+    begin
+        acum:= 0;
+        for j:=1 to M do
+            acum:= acum + MNotas[i,j];
+        Promedio:= acum / M;
+    end;
 
+procedure sitAlumno(var Promocionados: TV; VAlumno : TV; MNotas: TM; M,N:byte);
+    var
+        i, k: byte;
+        prom: real;
+
+    begin
+        k:= 0;
+
+        for i:=1 to N do
+            begin
+                prom:= Promedio(MNotas, i, M);
+                if prom >= 7 then
+                    begin
+                        k:= k + 1;
+                        Promocionados[k]:= VAlumno[i];
+                        
+                        writeln(VAlumno[i], ' Promociona con promedio ',prom:2:1);
+                        writeln;
+                    end
+                else if (5<=prom)and(prom<7) then
+                    begin
+                        writeln(VAlumno[i], ' Hablita con promedio ',prom:2:1);
+                        writeln;
+                    end
+                else
+                    begin
+                        writeln(VAlumno[i], ' Desaprueba con promedio ',prom:2:1);
+                        writeln;
+                    end;
+            end;
+
+        //muestro vector de Promocionados
+
+        for i:=1 to k do
+            writeln(Promocionados[i]);
+    end;
 
 begin
     cargaDatos(VAlumno, MNotas, N, M);
     Aprobados(VAlumno, MNotas, M, N);
+    writeln;
+    sitAlumno(Promocionados, VAlumno, MNotas, M, N);
     readln;
 end.
